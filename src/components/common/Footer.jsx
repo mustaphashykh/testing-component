c81495442f28cba43ee89ebaf154b4b5321a80ca
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2024-03-19 08:48:17 +0500

    Footer Done

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,21 @@
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
+import PropTypes from 'prop-types';
+
+function Footer({ icons }) {
+    const date = new Date()
+    const year = date.getFullYear()
+    return (
+        <div className="text-lg text-highlighted-text w-full flex items-center justify-between p-2 sm:p-0 sm:py-10">
+            <p className="font-medium drop-shadow">&copy; {year} IQTEST.AI</p>
+            <div className='flex items-center gap-4'>
+                {
+                    icons.length > 0 && icons.map((icon, idx) => <div key={idx} className='w-10 h-10 rounded-full text-white bg-main-purple flex items-center justify-center text-base'><i className={icon} /></div>)
+                }
+            </div>
+        </div>
+    )
+}
+Footer.propTypes = {
+    icons: PropTypes.array
 }
 
 export default Footer
