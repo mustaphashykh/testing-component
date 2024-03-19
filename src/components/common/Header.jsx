2a71b7649b31a1bb9b996750534de9e876c97005
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2024-03-19 07:31:30 +0500

    Header Responsiveness Done

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
     ];
     const [selectedOption, setSelectedOption] = useState(options[0]);
     return (
-        <div className="bg-white text-highlighted-text text-base w-full flex items-center justify-between py-8 px-10">
+        <div className="bg-white text-highlighted-text text-base w-full flex items-center justify-between p-2 sm:p-0 sm:py-8 sm:px-10">
             <div className='flex items-center gap-x-3'>
                 <div className='w-8 h-10 skew-x-[40deg] flex flex-wrap'>
                     <div className='w-[50%] h-[50%]' />
@@ -27,7 +27,7 @@ function Header() {
                 </div>
                 <p className="font-medium text-[1.15rem]">IQTEST.ai</p>
             </div>
-            <div className="flex items-center gap-x-20">
+            <div className="xl:flex items-center gap-x-20 hidden">
                 <ul className="flex items-center gap-x-24">
                     <li className="text-main-purple">Home</li>
                     <li>Start Test</li>
@@ -38,6 +38,11 @@ function Header() {
                     <Dropdown options={options} onChange={setSelectedOption} value={selectedOption} />
                     <button type="button" className="text-sm bg-main-purple text-white rounded-full px-7 py-3 transition-all duration-500 hover:bg-gray-300">Login</button>
                 </div>
+            </div>
+            <div className='w-8 flex flex-col gap-1.5 cursor-pointer xl:hidden'>
+                <div className='bar' />
+                <div className='bar' />
+                <div className='bar' />
             </div>
         </div>
     )
