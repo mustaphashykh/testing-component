4e49a469dd5b7b204f4c00bd1dcf54bac9d0c26d
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2024-03-19 08:48:46 +0500

    Width maintain

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Header } from "./components/common"
+import { Footer, Header } from "./components/common"
+import { DisplayCard } from "./components/extras"
 
 function App() {
   return (
     <>
       <Header />
-      this is react app
+      <div className="w-[95%] md:w-[91%] 2xl:w-[1200px] mx-auto">
+        <DisplayCard />
+        <Footer icons={["fa-brands fa-instagram", "fa-brands fa-facebook-f", "fa-brands fa-twitter", "fa-brands fa-linkedin-in"]} />
+      </div>
     </>
   )
 }
