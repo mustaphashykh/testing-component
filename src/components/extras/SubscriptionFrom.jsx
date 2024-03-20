d0bbe5c2f9b2cd075d541f461f51d28c5b6f35d6
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2024-03-21 03:23:43 +0500

    Input Set

--- src/components/extras/SubscriptionFrom.jsx
+++ src/components/extras/SubscriptionFrom.jsx
@@ -4,7 +4,7 @@ function SubscriptionFrom() {
             <form className="w-full text-center space-y-8" onSubmit={(e) => e.preventDefault()}>
                 <p className="text-xl font-semibold">Stay up to date</p>
                 <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-                    <input type="text" placeholder="Enter Your email" className="w-80 h-[3.7rem] px-6 rounded-full" />
+                    <input type="text" placeholder="Enter Your email" className="w-80 h-[3.7rem] px-6 rounded-full text-main-black outline-none" />
                     <button type="submit" className="w-80 md:w-40 h-[3.7rem] text-xl bg-main-purple text-white rounded-full">Subscribe</button>
                 </div>
                 <p className="text-lg">By subscribing you agree with our Privacy Policy</p>
